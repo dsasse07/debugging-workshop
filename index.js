f9d1620f5245b33c5823f9c9bdd5c38fc30fd3f7
Daniel Sasse <dsasse07@gmail.com>
2021-02-08 10:34:02 -0500

    Done

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  let username = document.getElementById('name-input').value
   let joke;
-
+  
   function fetchJoke(){
     fetch('https://icanhazdadjoke.com/', {
       headers: {
@@ -12,16 +12,22 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => renderJoke(jokeData.joke) )
   }
 
-  form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
+  const renderJoke = jokeText => {
+    joke = jokeText
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
     `
     jokeList.appendChild(newJokeLi)
+  }
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    username = document.getElementById('name-input').value
+    if(username === "") return;
+    fetchJoke()
+    
   })
 })
